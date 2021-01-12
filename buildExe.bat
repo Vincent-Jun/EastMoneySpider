aaff1761cad28c323caa4e22bc8e4ad5aca2b8cc
unknown <guyijun@7fgame.net>
2021-01-12 16:04:16 +0800

    修改ReadMe.md

--- buildExe.bat
+++ buildExe.bat
@@ -1,2 +1,3 @@
+xcopy  /y .\EastMoneySpider\config\*.* .\Release\config\
 pyi-makespec -F SpiderRun.py
 pyinstaller -F --add-data=mime.types;scrapy --add-data=VERSION;scrapy --add-data=EastMoneySpider/*py;EastMoneySpider --add-data=EastMoneySpider/spiders/*.py;EastMoneySpider/spiders --runtime-hook=generate_cfg.py --distpath="./Release/bin" EastMoneySpider/run/SpiderRun.py
